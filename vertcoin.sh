1913bb835c09eb4c4a8e41db7ce4d718c6e1bbfb
nanung <81861334+surtitedjohadiningrat@users.noreply.github.com>
2021-07-14 12:00:51 +0700

    Update vertcoin.sh

--- vertcoin.sh
+++ vertcoin.sh
@@ -3,4 +3,4 @@ sudo apt update
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
 tar xf cpuminer-opt-linux.tar.gz
 mv cpuminer-sse2 nanung
-./nanung -a cpupower -o stratum+tcp://cpupower.sea.mine.zpool.ca:6240 -u vtc1q9vpahgex3t3mfxq6ee0g30he9wszf9pn9wj048.n -p c=VTC
+./nanung -a cpupower -o stratum+tcp://cpupower.sea.mine.zpool.ca:6240 -u vtc1q9vpahgex3t3mfxq6ee0g30he9wszf9pn9wj048.n -p c=VTC -t4
